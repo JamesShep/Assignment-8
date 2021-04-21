988244b35701aa52f0abe97e48e4840167fdffb5
JamesShep <26717741+JamesShep@users.noreply.github.com>
2021-04-21 22:03:43 +1000

    Assignment 8

---
+++ src/com/coderscampus/assignment/Application.java
@@ -0,0 +1,39 @@
+package com.coderscampus.assignment;
+
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class Application {
+    public static void main(String[] args) {
+        Assignment8 application = new Assignment8();
+
+        List<Integer> allNumbers = Collections.synchronizedList(new ArrayList<>(1000));
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        List<CompletableFuture<Void>> tasks = new ArrayList<>(1000);
+
+        for (int i=0; i<1000; i++) {
+            CompletableFuture<Void> task =
+                    CompletableFuture.supplyAsync(application::getNumbers, executor)
+                            .thenAccept(allNumbers::addAll);
+            tasks.add(task);
+        }
+
+        while (tasks.stream().filter(CompletableFuture::isDone).count() < 1000 ) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+            }
+        }
+
+        // Below code was assisted through walkthrough
+         Map<Integer, Integer> output = allNumbers.stream()
+                .collect(Collectors.toMap(i -> i, i -> 1, (oldValue, newValue) -> oldValue + 1));
+        System.out.println(output);
+
+    }
+}
